570c72b180d8ad122941b1fbec03cb1bd5ad5745
asleon <asleon870830@gmail.com>
2024-03-25 12:29:40 -0300

    mi primer commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "MI PRIMERA TAREA",
+    description: "mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "MI SEGUNDA TAREA",
+    description: "mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "MI TERCERA TAREA",
+    description: "mi tercera tarea",
+  }
+];
